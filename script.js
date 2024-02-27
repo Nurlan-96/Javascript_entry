83f0e95f519b6f8c8e8b37357f14cf9a9314f3f7
Nurlan <nurlanea@code.edu.az>
2024-02-27 23:24:37 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,138 @@
+/* tek ededler */
+// let n = 1;
+// let m = 10;
+// let count=0;
+// let sum = 0;
+// while(n<m){
+// if(n%2>0){
+//     count++;
+//     sum+=n;
+// }
+// n++;
+// }
+// console.log(`count of odd numbers: ${count}`);
+// console.log(`sum of odd numbers: ${sum}`);
+/* tek ededler */
+
+/* sade ve ya murekkeb */
+    // let num = 79;
+    // let i=2;
+    // let count=0;
+    // while ((i<(num/2)) && (count==0)){
+    //     if(num%i==0){
+    //         count++;
+    //     }
+    //     i++;
+    // }
+    // if (count<1){
+    //     console.log("sade");
+    // }
+    // else console.log("murekkeb");
+/* sade ve ya murekkeb */
+
+/* quvvet */
+// let n =87;
+// let m=2;
+// let isPowerOf2 = false;
+// while (m < n) {
+//     if (n % m === 0) {
+//         m *= 2;
+//         isPowerOf2 = true;
+//     } else {
+//         isPowerOf2 = false;
+//         break;
+//     }
+// }
+
+// if (isPowerOf2) {
+//     console.log('is a power of 2');
+// } else if (n === 1) {
+//     console.log('is a power of 2 on 0th degree');
+// } else {
+//     console.log('is not a power of two');
+// }
+/* quvvet */
+
+/* mertebe */
+// let m = 56439;
+// let n = 10;
+// let count = 0;
+// while (n<m){
+// if(m/n>=1){
+//     count++;
+//     n*=10;
+// }
+// else{
+//     n*=10;
+// }
+// }
+// console.log(`eded ${count+1} mertebelidir`);
+/* mertebe */
+
+/* array */
+// let array = [1,2,3,4,5,6]
+// let sum = array[0]+array[array.length-1]
+// let sumall = 0;
+// let i =0;
+// while(i<array.length){
+//     sumall+=array[i]
+//     i++;
+// }
+// console.log(sum);
+// console.log(sumall);
+// let highest = array[0];
+// for (let index = 0; index < array.length; index++) {
+//     if (array[index]>highest){
+//          highest = array[index];
+//     }
+//     else{
+//         continue;
+//     }
+// }
+// console.log(highest);
+/* array */
+
+/* tek ve ya cut */
+// let n = 4;
+// if (n%2==0){
+//     console.log('cut eded');
+// }
+// else console.log('tek eded');
+/* tek ve ya cut */
+
+/* 3 ve 7 */
+// let n = 21;
+// if ((n%3==0) && (n%7==0)){
+// console.log('bolunur');
+// }
+// else console.log('bolunmur');
+/* 3 ve 7 */
+
+/* her ikisi cut */
+// let n = 20;
+// let m = 13;
+// if ((n%2==0) && (m%2==0)){
+//     console.log(n+m);
+// }
+/* her ikisi cut */
+
+/* temperatur */
+// let degree = 16;
+// if (degree>15){
+//     console.log('hava istidir');
+// }
+// else console.log('hava soyuqdur');
+/* temperatur */
+
+/* cut ededlerin hasili */
+// let m = 22;
+// let n = 1;
+// let product = 1;
+// while(n<=m){
+//     if (n%2==0){
+//         product*=n;
+//     }
+//     n++;
+// }
+// console.log(product);
+/* cut ededlerin hasili */
